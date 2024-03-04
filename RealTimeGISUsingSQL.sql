abfa9f73d6453ee7f80aee6dd1f83f2058d07250
Greg Mattis, GISP <41452820+gmattis1991@users.noreply.github.com>
2024-03-04 09:34:03 -0800

    Update RealTimeGISUsingSQL.sql

--- RealTimeGISUsingSQL.sql
+++ RealTimeGISUsingSQL.sql
@@ -1,42 +1,30 @@
-	--- This example is utilizing fire department AVL's where the GPS pings are stored in the database as x and y values which are int data types.
-  
-	--- Purge Existing Old Data
-	Delete from [Database].[Schema].[FeatureClass]
-	where unit not in (select unit from [Database].[Schema].[GPS_Table] where unit_type = 'f' and unit_kind in ('Supervisor','Engine','Truck','HazMat','Squad') and active_flag<>0)
-
 	--inserts new units
-	insert into [Database].[Schema].[FeatureClass] (ObjectID,Shape,GDB_GEOMATTR_DATA,Unit,active_flag,Lat,Long,unit_kind,CallID)
+	insert into [Database].[Schema].[FeatureClass] (ObjectID,Shape,GDB_GEOMATTR_DATA,Unit,Latitude,Longitude)
 	--- Auto Calculate ObjectID based on the number of records currently in the feature class
 	select case when (select count(*) from [Database].[Schema].[FeatureClass]) = 0
 	then row_number() over(order by unit)
 	else (select  max(objectid) from [Database].[Schema].[FeatureClass])+row_number() over(order by unit) end as ObjectID,
-	--- Calculates the shape blob based on in the x, y, and SRID
-	--- syntax is geometry::Point(X,Y,SRID)
-	geometry::Point(cast (x as decimal(15,1))/1000000,cast(y as decimal(15,1))/1000000,4326) as shape,
+	--- Calculates the shape blob based on in the x, y, and WKID
+	--- syntax is geometry::Point(X,Y,WKID)
+	geometry::Point(cast X,Y,4326) as shape,
 	null as GDB_GEOMATTR_DATA,
 	unit,
-	active_flag,
-	cast(y as decimal(15,1))/1000000 as Lat, 
-	cast(x as decimal(15,1))/1000000 as Long, 
-	unit_kind,
-	callid
+	Y as Latitude, 
+	X as Longitude, 
 	from [Database].[Schema].[GPS_Table]
-	where unit not in (select unit from [Database].[Schema].[FeatureClass]) AND unit_kind in ('Supervisor','Engine','Truck','HazMat','Squad') and agency = 'VFD'
-	and x <> 0
+	where unit not in (select unit from [Database].[Schema].[FeatureClass])
 
 	--updates existing units
-	update a
-	set a.lat = cast(b.y as decimal(15,1))/1000000,
-	a.long = cast(b.x as decimal(15,1))/1000000,
-	a.active_flag = b.active_flag,
-	a.unit_kind = b.unit_kind,
-	a.shape = geometry::Point(cast (b.x as decimal(15,1))/1000000,cast(b.y as decimal(15,1))/1000000,4326),
-	a.callid = b.callid
-	from [Database].[Schema].[FeatureClass] a
-	join [Database].[Schema].[GPS_Table] b
-	on a.unit = b.unit
-	where a.lat <> cast (b.y as decimal(15,1))/1000000
-	or a.long <> cast (b.x as decimal(15,1))/1000000
-	or a.active_flag <> b.active_flag
-	or a.unit_kind <> b.unit_kind
-	or a.callid <> b.callid
+	update GIS
+	set GIS.Latitude = GPS.Y,
+	GIS.Longitude = GPS.X,
+	GIS.shape = geometry::Point(GPS.X,GPS.Y,4326),
+	from [Database].[Schema].[FeatureClass] GIS
+	join [Database].[Schema].[GPS_Table] GPS
+	on GIS.unit = GPS.unit
+	where GIS.Latitude <> GPS.Y
+	or GIS.Longitude <> GPS.X
+	
+	--- Purge Existing Old Data
+	Delete from [Database].[Schema].[FeatureClass]
+	where unit not in (select unit from [Database].[Schema].[GPS_Table])
